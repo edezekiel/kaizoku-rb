2dc21ffcd99aff3c5229f4eb352bcf5dd3967f5d
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2018-11-19 08:38:35 -0500

    additional debugging and styling

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,15 +1,4 @@
 class CommandLineInterface
-  def pastel
-    pastel = Pastel.new
-    pastel
-  end
-
-  def separator
-    pastel
-    puts "                                                      "
-    puts pastel.bright_magenta("======================================================")
-    puts "                                                      "
-  end
 
   def exit_screen
     puts "Thank you for using Kaizoku!"
@@ -22,6 +11,7 @@ class CommandLineInterface
     exit_screen
   end
 
+
   def greeting_screen
     separator
     puts "Welcome to #{pastel.bright_cyan('Kaizoku')}!"
@@ -30,6 +20,10 @@ class CommandLineInterface
     puts "Type #{pastel.bright_cyan('list')} to see a list of gem categories."
     separator
     get_category_screen
+  end
+
+  def get_category_screen
+    get_category_validation
   end
 
   def get_category_validation
@@ -52,13 +46,26 @@ class CommandLineInterface
     get_subcategory_screen
   end
 
-  def get_category_screen
-    get_category_validation
-  end
 
-  def get_subcategory
+  def get_subcategory_screen
+    separator
+    puts "Please make a selection:"
+    puts "  "
+    puts "#{pastel.bright_cyan('[category]')} - enter one of the categories see gem subcategories."
+    puts "#{pastel.bright_cyan('back')} - go back to the main screen."
+    puts "#{pastel.bright_cyan('exit')} - go quit the app."
     separator
     input = gets.chomp
+    if input == "exit"
+      exit_screen
+    elsif input == "back"
+      greeting_screen
+    else
+      get_subcategory(input)
+    end
+  end
+
+  def get_subcategory(input)
     separator
     doc = Nokogiri::HTML(open("https://www.ruby-toolbox.com"))
     doc.css(".category-group").each do |category|
@@ -66,6 +73,8 @@ class CommandLineInterface
         if category.css("h3").text == input
           @category = category
           @subcategory = subcategory
+          puts "Here are the #{pastel.bright_cyan('subcategories')}, if any:"
+          puts "  "
           puts subcategory.css("a span").text
           get_gem_screen
         end
@@ -73,15 +82,27 @@ class CommandLineInterface
     end
   end
 
-  def get_subcategory_screen
-    puts "Type the category name to see gem subcategories."
-    get_subcategory
-  end
 
-  def gem_description
+  def get_gem_screen
+    separator
+    puts "Type the subcategory to see the best gem for that category."
+    puts "#{pastel.bright_cyan('back')} - go back to the main screen."
+    puts "#{pastel.bright_cyan('exit')} - go quit the app."
     separator
     input = gets.chomp
     separator
+    if input == "exit"
+      exit_screen
+    elsif input == "back"
+      greeting_screen
+    else
+      doc = gem_description(input)
+      gem_description_output(doc)
+    end
+
+  end
+
+  def gem_description(input)
     url = "https://www.ruby-toolbox.com" + @subcategory.css("a").attribute("href").value
     doc = Nokogiri::HTML(open(url))
     doc
@@ -90,17 +111,24 @@ class CommandLineInterface
   def gem_description_output(doc)
     puts "Here's the gem for the job!"
     puts "  "
-    puts "name: " + doc.css(".is-size-4").first.children.text
-    puts "score: " + doc.css(".score").first.css("span").text
-    puts "description: " + doc.css(".description.column").first.text
-    puts "github: " + doc.css(".links.column").first.css(".button.is-white")[1].attributes["href"].text
+    puts "#{pastel.bright_cyan('name: ')}" + doc.css(".is-size-4").first.children.text
+    puts "#{pastel.bright_cyan('score: ')} " + doc.css(".score").first.css("span").text
+    puts "#{pastel.bright_cyan('description: ')} " + doc.css(".description.column").first.text
+    puts "#{pastel.bright_cyan('github: ')}: " + doc.css(".links.column").first.css(".button.is-white")[1].attributes["href"].text
+    separator
     exit_screen
   end
 
-  def get_gem_screen
-    puts "Type the subcategory to see the best gem for that category."
-    doc = gem_description
-    gem_description_output(doc)
+  def pastel
+    pastel = Pastel.new
+    pastel
+  end
+
+  def separator
+    pastel
+    puts "                                                      "
+    puts pastel.bright_magenta("======================================================")
+    puts "                                                      "
   end
 
   def run
